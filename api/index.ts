c3fca40262c3d9f4293f1bb22c7b469b79690be9
Sridhar R <srid70832@gmail.com>
2026-08-16 19:01:25 +0530

    fix: restore Vercel API server entry

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,4 @@
-import app from '../server.js';
+import app from '../server.ts';
 
 app.use((err: any, req: any, res: any, next: any) => {
   res.status(500).json({ success: false, error: { message: err.message, stack: err.stack } });
